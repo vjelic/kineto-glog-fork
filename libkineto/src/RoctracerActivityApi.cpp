570158f52581767350563a01537e645be6468d59
blorange-amd <bo.li2@amd.com>
2023-04-07 20:08:09 +0000

    Fixed for SWDEV-388439 - TestProfilerTree.test_profiler_experimental_tree_with_memory_and_stack
    Created for release/1.13 branch

--- libkineto/src/RoctracerActivityApi.cpp
+++ libkineto/src/RoctracerActivityApi.cpp
@@ -465,6 +465,7 @@ void RoctracerActivityApi::enableActivities(
     loggedIds_.add("hipPeekAtLastError");
     loggedIds_.add("hipModuleGetFunction");
     loggedIds_.add("hipEventCreateWithFlags");
+    loggedIds_.add("hipGetDeviceCount");
 
     // Enable API callbacks
     if (loggedIds_.invertMode() == true) {
